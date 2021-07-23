33df60ec8a704fb6a8adfefcde5d925ccc9de7ec
ninest <parth.kabra@gmail.com>
2021-07-24 01:04:18 +0800

    Add locations list

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import map from "@/public/map.png";
-
-
+import { locationsGroupList } from "@/data/map";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function MapPage() {
+  const [locationListId, setLocationListId] = useState(0);
   return (
     <>
       <div style={{ height: "100vh", overflow: "none" }}>
@@ -16,7 +18,7 @@ export default function MapPage() {
         >
           <TransformComponent>
             <div
-              style={{ height: "100vh", width: "100%" }}
+              style={{ height: "65vh", width: "100%" }}
               className="flex items-center justify-center"
             >
               {/* <img
@@ -27,8 +29,41 @@ export default function MapPage() {
               <Image src={map} alt="Northeastern campus map"></Image>
             </div>
           </TransformComponent>
-          <div className="absolute bottom-base right-base w-28 bg-white p-base rounded-md border shadow">
-            Legend and keys
+
+          <div className="absolute bottom-0 right-0 left-0 w-full h-2/5 overflow-y-scroll bg-white border-t">
+            <section className="p-base sticky top-0 bg-white flex space-x-base overflow-x-scroll">
+              {locationsGroupList.map((locationsGroupList, index) => (
+                <h3
+                  className={clsx(
+                    "whitespace-nowrap font-bold -m-xs p-xs rounded-md",
+                    {
+                      "bg-gray-lightest": locationListId == index,
+                    }
+                  )}
+                  style={{ color: locationsGroupList.color }}
+                  onClick={() => setLocationListId(index)}
+                >
+                  {locationsGroupList.name}
+                </h3>
+              ))}
+            </section>
+            <section className="p-base">
+              <ul>
+                {locationsGroupList[locationListId].locations.map(
+                  (location) => (
+                    <li className="flex items-center justify-between space-x-sm">
+                      <div className="space-x-sm">
+                        <span className="text-gray font-light">
+                          {location.coords}
+                        </span>
+                        <span>{location.name}</span>
+                      </div>
+                      <div className="font-bold">{location.shortName}</div>
+                    </li>
+                  )
+                )}
+              </ul>
+            </section>
           </div>
         </TransformWrapper>
       </div>
--- scripts/map.py
+++ scripts/map.py
@@ -2,11 +2,34 @@
 Copy-paste locations from map PDF, generate list of JS objects
 """
 
-locations_string = """C3 Columbus Parking Garage (CPG)
-E2 Belvidere Parking Garage (BPG)
-C2 Gainsborough Parking Garage (GPG)
-A2 Renaissance Park Parking Garage (RPG)
-A2 West Village Parking Garage (WPG)"""
+locations_string = """B3 10 Coventry (CV)
+C1 106–122 Levine (106-122) 
+C1 142–148 Hemenway St (142)
+C1 153 Hemenway St (LF)
+C2 319 Huntington Ave (319)
+C2 337 Huntington Ave (337) 
+B1 407 Huntington Ave (407) 
+B3 768 Columbus Ave (768) 
+B3 780 Columbus Ave (780) 
+B1 Burstein Hall (BU)
+C3 Davenport Commons A, B (DC)
+C2 East Village (EV)
+C2 Hastings Hall (HS)
+A2 International Village (IV)
+D1 Kennedy Hall (KY) 
+C1 Kerr Hall (KH)
+D1 Light Hall (LH)
+C1 Loftman Hall (LF)
+C1 Melvin Hall (MH)
+B1 Rubenstein Hall (RU) 
+D1 Smith Hall (SM)
+C1 Speare Hall (SP)
+C1 Stetson East (SE)
+C1 Stetson West (SW)
+A1 West Village A, B, C, E (WV) 
+B1 West Village F, G, H (WV) 
+B1 White Hall (WH)
+B1 Willis Hall (WI)"""
 
 for location in locations_string.split("\n"):
     coords = location.split(" ")[0]
