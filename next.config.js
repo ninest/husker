35808a2dc8ffde79be7fe2ee6d0fdf61bbceb97e
ninest <parth.kabra@gmail.com>
2021-08-19 21:28:42 +0800

    Add book downloads page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   experimental: { esmExternals: true },
+  async redirects() {
+    return [
+      {
+        source: "/books",
+        destination: "/resources/download-books",
+        permanent: true,
+      },
+    
+    ];
+  },
 };
