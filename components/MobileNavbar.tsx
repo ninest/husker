e9fdd2bbc06fb5c64630189db17d92575eb3094d
ninest <parth.kabra@gmail.com>
2022-01-03 02:32:15 +0800

    Add highlight links to navbar

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -1,4 +1,7 @@
+import { highlightedSidebarLinks } from "@/content/sidebar";
+import { IconId } from "@/types/icon";
 import { Icon } from "./Icon";
+import { SmartLink } from "./SmartLinks";
 
 interface MobileNavbarProps {
   onMenuClick: () => void;
@@ -7,14 +10,41 @@ export const MobileNavbar = ({ onMenuClick }: MobileNavbarProps) => {
   return (
     <header className="bg-light  px-md py-base border-b flex items-center justify-between">
       <div className="flex items-center">
-        <button className="text-gray border p-2 rounded mr-base" onClick={onMenuClick}>
+        <button
+          className="text-gray border p-2 rounded mr-base"
+          onClick={onMenuClick}
+        >
           <Icon id="griplines"></Icon>
         </button>
-        <div className="font-display font-black text-lg text-dark">Husker</div>
+        <SmartLink
+          href="/"
+          className="font-display font-black text-lg text-dark"
+        >
+          Husker
+        </SmartLink>
       </div>
-      <button className="text-gray border p-2 rounded-full" onClick={onMenuClick}>
-        <Icon id="search"></Icon>
-      </button>
+
+      <div className="flex items-center space-x-xs">
+        <div className="w-40 overflow-hidden flex justify-end ">
+          {highlightedSidebarLinks.map((link) => {
+            return (
+              <SmartLink
+                href={link.href}
+                className="hover:bg-gray-100 p-2 rounded"
+              >
+                <Icon id={link.icon as IconId} />
+              </SmartLink>
+            );
+          })}
+        </div>
+
+        <button
+          className="text-gray border p-2 rounded-full"
+          onClick={onMenuClick}
+        >
+          <Icon id="search"></Icon>
+        </button>
+      </div>
     </header>
   );
 };
