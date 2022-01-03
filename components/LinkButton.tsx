f585598ef97168b2cbee9b38b6c42cddfd386b9a
ninest <parth.kabra@gmail.com>
2022-01-04 02:38:31 +0800

    Keep width of icons constant for LinkButton

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -17,7 +17,11 @@ export const LinkButton = ({
       className="flex items-center space-x-md rounded p-base bg-gray-100 hover:bg-gray-200 "
       href={link.href}
     >
-      <Icon id={link.icon} className="text-sm"></Icon>
+      <Icon
+        id={link.icon}
+        className="text-sm"
+        style={{ width: "1.25em" }}
+      ></Icon>
       <div>
         <div className="font-semibold text-sm text-gray">{link.name}</div>
         {showDescription && (
