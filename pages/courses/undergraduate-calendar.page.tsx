d125b5ebf873de63e4f613156313186425b24e91
ninest <parth.kabra@gmail.com>
2022-01-22 10:46:35 -0500

    Remove old code

--- pages/courses/undergraduate-calendar.page.tsx
+++ pages/courses/undergraduate-calendar.page.tsx
@@ -14,34 +14,6 @@ const UndergraduateCalendarPage = () => {
         title="Undergraduate Calendar 2021-2022"
       ></ArticleHead>
 
-      {/* <object
-        className="w-full"
-        style={{
-          height: "calc(100vh - 10rem)",
-        }}
-        data={calendarHref}
-        type="application/pdf"
-      >
-        <embed src={calendarHref} type="application/pdf" />
-      </object> */}
-
-      {/* <object
-        data={calendarHref}
-        type="application/pdf"
-        className="w-full"
-        style={{
-          height: "calc(100vh - 10rem)",
-        }}
-      >
-        <embed
-          className="w-full"
-          style={{
-            height: "calc(100vh - 10rem)",
-          }}
-          src={`https://docs.google.com/viewer?url=https://husker.vercel.app/${calendarHref}&embedded=true`}
-        />
-      </object> */}
-
       <PDFEmbed href={calendarHref}></PDFEmbed>
 
       <Spacer />
