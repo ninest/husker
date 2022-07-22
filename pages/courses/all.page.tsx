b9eb1848dbe90f59379b8fa0b753def1e589a3e7
ninest <parth.kabra@gmail.com>
2022-07-22 22:22:56 +0800

    Add all subjects page

---
+++ pages/courses/all.page.tsx
@@ -0,0 +1,53 @@
+import { ArticleHead } from "@/components/ArticleHead";
+import { Block } from "@/components/Block";
+import { Debug } from "@/components/util/Debug";
+import { Grid } from "@/components/util/Grid";
+import { readFile } from "@/lib/file/read";
+import { Course, Subject } from "@/types/courses";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { NextSeo } from "next-seo";
+import subjects from "../../.raw/subjects.json";
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  return {
+    props: { subjects },
+  };
+};
+
+interface AllCoursesPageProps {
+  subjects: Subject[];
+}
+
+const AllCoursesPage = ({ subjects }: AllCoursesPageProps) => {
+  return (
+    <>
+      <NextSeo
+        title={"Subjects"}
+        description={`List of subjects at Northeastern University`}
+      />
+
+      <ArticleHead
+        backButtonHref={`/courses/`}
+        backButtonText={"Courses"}
+        title={"All Subjects"}
+      />
+
+      <div className="wrapper">
+        <Grid className="md:grid-cols-2">
+          {subjects.map((subject) => (
+            <Block
+              key={subject.code}
+              id={subject.code}
+              title={`${subject.description}`}
+              href={`/courses/${subject.code}`}
+            >{subject.code}</Block>
+          ))}
+        </Grid>
+        <Debug data={subjects} />
+      </div>
+    </>
+  );
+};
+
+
+export default AllCoursesPage
