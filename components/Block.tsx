5f4f09890667086c15cd1dd5ee365a50173a41e0
ninest <parth.kabra@gmail.com>
2022-07-24 03:03:00 +0800

    Improve block component

--- components/Block.tsx
+++ components/Block.tsx
@@ -13,18 +13,18 @@ export const Block = ({ title, children, href, ...props }: BlockProps) => {
   const isClickable = !!href;
   const className = clsx(
     props.className,
-    "block p-base rounded transition-colors bg-gray-50 dark:bg-gray-900",
+    "block p-sm rounded transition-colors bg-gray-50 dark:bg-gray-900",
     {
       "hover:bg-gray-100 dark:hover:bg-gray-800": isClickable,
     }
   );
   const content = (
     <>
-      <div className="font-semibold text-sm text-gray">{title}</div>
+      <div className="font-semibold text-sm text-gray dark:text-gray-light">{title}</div>
       {children && (
         <>
           <Spacer size="xs"></Spacer>
-          <div className="text-xs text-gray-light dark:text-gray-dark">{children}</div>
+          <div className="text-xs text-gray-light dark:text-gray">{children}</div>
         </>
       )}
     </>
