9fac4aa0612745797ae66602fa745b3dc717f19b
ninest <parth.kabra@gmail.com>
2022-08-09 21:54:21 +0800

    Add sections count

--- components/courses/sections/SectionsList.tsx
+++ components/courses/sections/SectionsList.tsx
@@ -12,19 +12,35 @@ interface SectionsListProps {
 export const SectionsList = ({ course }: SectionsListProps) => {
   const { sections, isLoading } = useSections(course.sections);
 
+  const totalSections = sections?.length;
+  const totalSectionsWithSeats = sections?.filter((section) => section.seats.available > 0).length;
+
   return (
     <div>
       {activeTerms.map((term) => {
         const sectionsForTerm = sections?.filter((section) => section.term === term.code);
         return (
           <section key={term.code}>
-            <h3 className="font-bold text-lg">{term.description}</h3>
-            <Spacer size="sm" />
-            <div className="space-y-sm">
-              {sectionsForTerm?.map((section, index) => (
-                <Section key={index} section={section} />
-              ))}
+            <div className="flex items-baseline space-x-base">
+              <h3 className="font-bold text-lg">{term.description}</h3>
+              {!isLoading && (
+                <div>
+                  <span className="font-mono text-sm">{totalSections}</span> sections,{" "}
+                  <span className="font-mono text-sm">{totalSectionsWithSeats}</span> sections with
+                  seats
+                </div>
+              )}
             </div>
+            {!isLoading && (
+              <>
+                <Spacer size="sm" />
+                <div className="space-y-sm">
+                  {sectionsForTerm?.map((section, index) => (
+                    <Section key={index} section={section} />
+                  ))}
+                </div>
+              </>
+            )}
           </section>
         );
       })}
