13a021c9758ac0314b9692b209d1d3a14ddb1656
ninest <parth.kabra@gmail.com>
2022-08-09 22:03:21 +0800

    Use disclosure to make sections expandable

--- components/courses/sections/SectionsList.tsx
+++ components/courses/sections/SectionsList.tsx
@@ -2,6 +2,8 @@ import { activeTerms } from "@/content/terms";
 import { useSections } from "@/hooks/sections";
 import { pluralize } from "@/lib/string";
 import { Course } from "@/types/courses";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { Disclosure } from "@headlessui/react";
 import { Debug } from "../../util/Debug";
 import { Spacer } from "../../util/Spacer";
 import { Section } from "./Section";
@@ -18,36 +20,40 @@ export const SectionsList = ({ course }: SectionsListProps) => {
     sections?.filter((section) => section.seats.available > 0).length ?? 0;
 
   return (
-    <div>
-      {activeTerms.map((term) => {
+    <div className="space-y-md">
+      {[...activeTerms, ...activeTerms].map((term) => {
         const sectionsForTerm = sections?.filter((section) => section.term === term.code);
+        const [parent] = useAutoAnimate<HTMLDivElement>({ duration: 100 });
         return (
-          <section key={term.code}>
-            <div className="flex items-baseline space-x-base">
-              <h3 className="font-bold text-lg">{term.description}</h3>
-              {!isLoading && (
-                <div>
-                  <span className="font-mono text-sm">{totalSections}</span>{" "}
-                  {pluralize(totalSections, "section")},{" "}
-                  <span className="font-mono text-sm">{totalSectionsWithSeats}</span>{" "}
-                  {pluralize(totalSectionsWithSeats, "section")} with seats
-                </div>
-              )}
-            </div>
-            {!isLoading && (
-              <>
-                <Spacer size="sm" />
-                <div className="space-y-sm">
-                  {sectionsForTerm?.map((section, index) => (
-                    <Section key={index} section={section} />
-                  ))}
-                </div>
-              </>
-            )}
+          <section key={term.code} ref={parent}>
+            <Disclosure>
+              <Disclosure.Button className="flex items-baseline space-x-base">
+                <h3 className="font-bold text-lg">{term.description}</h3>
+                {!isLoading && (
+                  <div>
+                    <span className="font-mono text-sm">{totalSections}</span>{" "}
+                    {pluralize(totalSections, "section")},{" "}
+                    <span className="font-mono text-sm">{totalSectionsWithSeats}</span>{" "}
+                    {pluralize(totalSectionsWithSeats, "section")} with seats
+                  </div>
+                )}
+              </Disclosure.Button>
+              <Disclosure.Panel>
+                {!isLoading && (
+                  <>
+                    <Spacer size="sm" />
+                    <div className="space-y-sm">
+                      {sectionsForTerm?.map((section, index) => (
+                        <Section key={index} section={section} />
+                      ))}
+                    </div>
+                  </>
+                )}
+              </Disclosure.Panel>
+            </Disclosure>
           </section>
         );
       })}
-      <Debug data={sections} />
     </div>
   );
 };
