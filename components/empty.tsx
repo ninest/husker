ffb4f1e8691e49d61e39fb40278a9df20c18845a
ninest <parth.kabra@gmail.com>
2023-12-08 12:18:30 -0500

    Display favorites and animation

--- components/empty.tsx
+++ components/empty.tsx
@@ -5,7 +5,7 @@ interface EmptyProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const Empty = ({ children, className }: EmptyProps) => {
   return (
-    <div className={cn(className, "border-2 border-dashed dark:border-gray-800 p-base rounded-lg text-gray-400")}>
+    <div className={cn(className, "border-2 border-dashed dark:border-gray-800 p-base rounded-lg text-gray-400 p-5")}>
       {children}
     </div>
   );
