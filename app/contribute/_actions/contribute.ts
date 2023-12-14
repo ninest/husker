8812ffc45e1ed44bb13b5cfdddd462a1993eb26b
ninest <parth.kabra@gmail.com>
2023-12-14 00:50:38 -0500

    Fix newline in forms

--- app/contribute/_actions/contribute.ts
+++ app/contribute/_actions/contribute.ts
@@ -3,7 +3,9 @@
 import { ContributeFormType } from "@/app/contribute/contribute-form";
 
 export async function contributeAction(params: ContributeFormType) {
-  const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSdQ8vhyic8Z5lxnBw9643UnqPxN2MIfssLYz32OBW_Vhn_X9A/formResponse?usp=pp_url&entry.770504043=${params.name}&entry.1613298240=${params.content}&entry.1321358172=${params.credit}`;
+  const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSdQ8vhyic8Z5lxnBw9643UnqPxN2MIfssLYz32OBW_Vhn_X9A/formResponse?usp=pp_url&entry.770504043=${
+    params.name
+  }&entry.1613298240=${encodeURIComponent(params.content)}&entry.1321358172=${params.credit}`;
   try {
     const response = await fetch(formUrl);
     return { type: "success" };
